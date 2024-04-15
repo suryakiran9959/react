55fce332ce1b08d0851ff18e711a0a980fff5b45
suryakiran9959 <surya.mail.net@gmail.com>
2024-04-15 12:39:57 +0530

    added stop,reset button

--- src/hooks/useState/index2.jsx
+++ src/hooks/useState/index2.jsx
@@ -2,16 +2,33 @@ import { useState } from "react"
 
 const UseStateExp3=()=>{
     const[timer,setTimer]=useState(20)
+    const [reset,setReset]=useState(null)
+    const [isRunning,setIsRunning]=useState(false)
 
-    const startTimer=()=>{
-        setInterval(()=>{
-            setTimer(preTime=>preTime-1)
+    const startTimer=()=>{ 
+    if(!isRunning){
+     const time=setInterval(()=>{
+        setTimer(preTime=>preTime-1)
         },1000)
+       setReset(time)
+       setIsRunning(true)
     }
+}
+const stopTimer=()=>{
+    clearInterval(reset)
+    setIsRunning(false)
+}
+const resetTimer=()=>{
+    clearInterval(reset)
+    setTimer(20)
+    setIsRunning(false)
+}
     return(
         <>
         <h1>useStateExp3</h1>
         <button onClick={startTimer}>Start Timer</button>
+        <button onClick={resetTimer}>Reset</button>
+        <button onClick={stopTimer}>Stop</button>
         <h1 style={{
             color:timer<=10 ? "red":"black"
         }}>{timer}</h1>
@@ -22,3 +39,5 @@ const UseStateExp3=()=>{
     )
 }
 export default UseStateExp3
+
+
