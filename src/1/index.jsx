acaccac429da66074f4a27cba992845f3ccb4e8e
suryakiran9959 <surya.mail.net@gmail.com>
2024-04-17 12:25:39 +0530

    s

---
+++ src/1/index.jsx
@@ -0,0 +1,10 @@
+const { useState } = require("react")
+
+function RandomNumbers(){
+    const [number,setNumber]=useState({})
+    return(
+        <>
+        <h1>Random NUmbers Generator</h1>
+        </>
+    )
+}
