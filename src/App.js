edeef7c6a945f3d215e8a362f269d6bc70f3cdc4
suryakiran9959 <surya.mail.net@gmail.com>
2024-06-12 15:59:46 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Props from './props';
 
 function App() {
   return (
-    
+    <>
+    <Props name={"login"}/>
+    <Props name={"SignUp"}/>
+    </>
   );
 }
 
---
+++ src/props.jsx
@@ -0,0 +1,8 @@
+function Props ({name}){
+    return(
+        <>
+        <button>{name}</button>
+        </>
+    )
+}
+export default Props
