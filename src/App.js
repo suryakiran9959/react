53e2d0608f09d07f0c59b7f3c38b9d0f8aff806c
suryakiran9959 <surya.mail.net@gmail.com>
2024-07-05 22:20:35 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 
 import UseStateExp from './1';
-import Parent from './2';
-import Child from './3';
-import Counter from './4';
-
 
 import './App.css';
-import UseEffectExp from './useEffect';
-import UseRefExp from './useRef';
+
 
 function App() {
   return (
     <>
-  <Counter/>
+ 
     </>
   );
 }
