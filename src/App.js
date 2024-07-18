783f162c2e7d21466b148280056018f658d90cb4
suryakiran9959 <surya.mail.net@gmail.com>
2024-07-18 12:45:13 +0530

    s

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Home } from "./component/home";
 import { Provider } from "react-redux";
 import { reduxStore } from "./redux/store/store";
 import { Home1 } from "./component/home1";
+import { Form1 } from "./form";
+
 
 function App() {
   return (
     <>
-      <Provider store={reduxStore}>
+      {/* <Provider store={reduxStore}>
         <Home />
         <Home1/>
-      </Provider>
+      </Provider> */}
+      <Form1/>
     </>
   );
 }
