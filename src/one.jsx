01d19edc8339265c07360bf85e588c8c1ae1f43f
suryakiran9959 <surya.mail.net@gmail.com>
2024-09-30 15:12:20 +0530

    s

--- src/one.jsx
+++ src/one.jsx
@@ -1,2 +1,21 @@
+import { useMemo } from "react";
+import { useState } from "react";
 
+function One(){
+    const[num,setNum]=useState(0)
+    const[data,setData]=useState(0)
 
+    const memVal= useMemo(()=>{
+        console.log("useMemo rendering..");
+        return data*10
+    },[data])
+
+    return(
+        <>
+        <input type="text"  onChange={(e)=>setNum(e.target.value)} />
+        <button onClick={()=> setData(num)}>set</button>
+        <p>memorized value:{memVal}</p>
+        </>
+    )
+}
+export default One;
